055ffe231bf9a84968322ef54c82721381aa380b
Alban Jubert <alban@lahautesociete.com>
2018-03-08 12:52:04 +0100

    Added missing files

---
+++ src/assetbundles/elasticsearchutility/dist/js/Elasticsearch.js
@@ -0,0 +1,11 @@
+/**
+ * Elasticsearch plugin for Craft CMS
+ *
+ * Elasticsearch Utility JS
+ *
+ * @author    La Haute Société
+ * @copyright Copyright (c) 2018 La Haute Société
+ * @link      https://www.lahautesociete.com
+ * @package   Elasticsearch
+ * @since     1.0.0
+ */
---
+++ src/utilities/ElasticsearchUtilities.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Elasticsearch plugin for Craft CMS 3.x
+ *
+ * Bring the power of Elasticsearch to you Craft 3 CMS project
+ *
+ * @link      https://www.lahautesociete.com
+ * @copyright Copyright (c) 2018 La Haute Société
+ */
+
+namespace lhs\elasticsearch\utilities;
+
+use Craft;
+use craft\base\Utility;
+use lhs\elasticsearch\assetbundles\elasticsearchutility\ElasticsearchUtilityAsset;
+
+/**
+ * Elasticsearch Utility
+ *
+ * Utility is the base class for classes representing Control Panel utilities.
+ *
+ * https://craftcms.com/docs/plugins/utilities
+ *
+ * @author    La Haute Société
+ * @package   Elasticsearch
+ * @since     1.0.0
+ */
+class ElasticsearchUtilities extends Utility
+{
+    // Static
+    // =========================================================================
+
+    /**
+     * Returns the display name of this utility.
+     *
+     * @return string The display name of this utility.
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('elasticsearch', 'Elasticsearch');
+    }
+
+    /**
+     * Returns the utility’s unique identifier.
+     *
+     * The ID should be in `kebab-case`, as it will be visible in the URL (`admin/utilities/the-handle`).
+     *
+     * @return string
+     */
+    public static function id(): string
+    {
+        return 'elasticsearch-utilities';
+    }
+
+    /**
+     * Returns the path to the utility's SVG icon.
+     *
+     * @return string|null The path to the utility SVG icon
+     */
+    public static function iconPath()
+    {
+        return Craft::getAlias("@lhs/elasticsearch/assetbundles/elasticsearchutility/dist/img/icon.svg");
+    }
+
+    /**
+     * Returns the number that should be shown in the utility’s nav item badge.
+     *
+     * If `0` is returned, no badge will be shown
+     *
+     * @return int
+     */
+    public static function badgeCount(): int
+    {
+        return 0;
+    }
+
+    /**
+     * Returns the utility's content HTML.
+     *
+     * @return string
+     */
+    public static function contentHtml(): string
+    {
+        Craft::$app->getView()->registerAssetBundle(ElasticsearchUtilityAsset::class);
+
+        $someVar = 'Have a nice day!';
+        return Craft::$app->getView()->renderTemplate(
+            'elasticsearch/_components/utilities/Elasticsearch_content'
+        );
+    }
+}
